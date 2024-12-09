bd61dc724ca845daaeec3656e921d94779a445db
prince <princeruselbstamaria@gmail.com>
2024-12-09 15:24:32 +0800

    first

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<?php
+    include 'connection.php';
+    $result = $conn->query("SELECT * FROM Song");
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="stlye.css">
+    <title>Song</title>
+</head>
+<body>
+    <video autoplay muted loop class="video-background">
+        <source src="img/bg.mp4" type="video/mp4">
+    </video>
+    <div class="overlay">
+        <div class="container">
+            <div class="leftside">
+                <h1>PlaceHolder</h1>
+            </div>
+            <div class="rightside">
+                <div class="title">
+                    <h1>Top<br>Songs</h1>
+                </div>
+                <div class="listofsong">
+                    <table>
+                        <?php
+                        if($result->num_rows > 0){
+                            while($row = $result->fetch_assoc()){
+                                echo "<tr>";
+                                echo "<td>
+                                        <div class='songImg'>
+                                            <img src='" . 
+                                            (!empty($row['image_path']) ? $row['image_path'] : 'img/cover.jpg') . 
+                                            "' alt='Album Cover'>
+                                        </div>
+                                    </td>";
+                                echo "<td>
+                                        <div class='song-info'>
+                                            <div class='SongName'>{$row['name']}</div>
+                                            <div class='Artist'>{$row['artist']}</div>
+                                        </div>
+                                     </td>";
+                                echo "<td>
+                                        <div class='tableButton'>
+                                            <button class='tbbutn'><img src='img/delete-bin-2-fill.svg' alt='Delete'></button>
+                                            <button class='tbbutn'><img src='img/edit-2-fill.svg' alt='Edit'></button>
+                                        </div>
+                                      </td>";
+                                echo "</tr>";
+                            }
+                        }
+                        ?>
+                    </table>
+                </div>
+                
+                <!-- Star button moved outside listofsong -->
+            <button class="starbut" onclick="window.location.href='form.php'">
+                Add Song
+                <div class="star-1">
+                    <svg
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 784.11 815.53"
+                    style="
+                        shape-rendering: geometricPrecision;
+                        text-rendering: geometricPrecision;
+                        image-rendering: optimizeQuality;
+                        fill-rule: evenodd;
+                        clip-rule: evenodd;
+                    "
+                    version="1.1"
+                    xml:space="preserve"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <defs></defs>
+                    <g id="Layer_x0020_1">
+                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                        <path
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                        class="fil0"
+                        ></path>
+                    </g>
+                    </svg>
+                </div>
+                <div class="star-2">
+                    <svg
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 784.11 815.53"
+                    style="
+                        shape-rendering: geometricPrecision;
+                        text-rendering: geometricPrecision;
+                        image-rendering: optimizeQuality;
+                        fill-rule: evenodd;
+                        clip-rule: evenodd;
+                    "
+                    version="1.1"
+                    xml:space="preserve"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <defs></defs>
+                    <g id="Layer_x0020_1">
+                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                        <path
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                        class="fil0"
+                        ></path>
+                    </g>
+                    </svg>
+                </div>
+                <div class="star-3">
+                    <svg
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 784.11 815.53"
+                    style="
+                        shape-rendering: geometricPrecision;
+                        text-rendering: geometricPrecision;
+                        image-rendering: optimizeQuality;
+                        fill-rule: evenodd;
+                        clip-rule: evenodd;
+                    "
+                    version="1.1"
+                    xml:space="preserve"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <defs></defs>
+                    <g id="Layer_x0020_1">
+                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                        <path
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                        class="fil0"
+                        ></path>
+                    </g>
+                    </svg>
+                </div>
+                <div class="star-4">
+                    <svg
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 784.11 815.53"
+                    style="
+                        shape-rendering: geometricPrecision;
+                        text-rendering: geometricPrecision;
+                        image-rendering: optimizeQuality;
+                        fill-rule: evenodd;
+                        clip-rule: evenodd;
+                    "
+                    version="1.1"
+                    xml:space="preserve"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <defs></defs>
+                    <g id="Layer_x0020_1">
+                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                        <path
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                        class="fil0"
+                        ></path>
+                    </g>
+                    </svg>
+                </div>
+                <div class="star-5">
+                    <svg
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 784.11 815.53"
+                    style="
+                        shape-rendering: geometricPrecision;
+                        text-rendering: geometricPrecision;
+                        image-rendering: optimizeQuality;
+                        fill-rule: evenodd;
+                        clip-rule: evenodd;
+                    "
+                    version="1.1"
+                    xml:space="preserve"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <defs></defs>
+                    <g id="Layer_x0020_1">
+                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                        <path
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                        class="fil0"
+                        ></path>
+                    </g>
+                    </svg>
+                </div>
+                <div class="star-6">
+                    <svg
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 784.11 815.53"
+                    style="
+                        shape-rendering: geometricPrecision;
+                        text-rendering: geometricPrecision;
+                        image-rendering: optimizeQuality;
+                        fill-rule: evenodd;
+                        clip-rule: evenodd;
+                    "
+                    version="1.1"
+                    xml:space="preserve"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <defs></defs>
+                    <g id="Layer_x0020_1">
+                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+                        <path
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                        class="fil0"
+                        ></path>
+                    </g>
+                    </svg>
+                </div>
+                </button>
+
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,82 @@
+<?php
+include 'connection.php';
+
+$name = $_POST['song'];
+$artist = $_POST['artist'];
+
+// File upload handling
+if(isset($_FILES['coverImage'])) {
+    $file = $_FILES['coverImage'];
+    $fileName = $file['name'];
+    $fileTmpName = $file['tmp_name'];
+    $fileError = $file['error'];
+    
+    $uploadDir = 'uploads/';
+    if (!file_exists($uploadDir)) {
+        mkdir($uploadDir, 0777, true);
+    }
+    
+    $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    $uniqueFileName = uniqid() . '.' . $fileExt;
+    $fileDestination = $uploadDir . $uniqueFileName;
+    
+    if($fileError === 0) {
+        if(move_uploaded_file($fileTmpName, $fileDestination)) {
+            if (isset($name) && isset($artist)){
+                $sql = "INSERT INTO Song (name, artist, image_path) 
+                        VALUES ('$name', '$artist', '$fileDestination')";
+                                if ($conn->query($sql) === TRUE) {
+                                    $uploadSuccess = true;
+                                } else {
+                                    $errorMessage = "Error: " . $conn->error;
+                                }
+
+            }
+            
+        }
+    }
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stlye.css">
+</head>
+<body>
+    <video autoplay muted loop class="video-background">
+        <source src="img/bg.mp4" type="video/mp4">
+    </video>
+<div class="overlay">
+    <button class="exitbut" href="index.php" onclick="window.location.href='index.php'">
+        <img src="img/close.svg" alt="exit">
+    </button>
+    <div class="container">
+        <div class="messconf">
+            <div class="confirmation">
+                <h1>
+                <?php if($uploadSuccess): ?>
+                            Song Successfully Added
+                            <script>
+                                setTimeout(function() {
+                                    window.location.href = 'index.php';
+                                }, 3000);
+                            </script>
+                        <?php else: ?>
+                            <?php echo $errorMessage; ?>
+                        <?php endif; ?>
+                </h1>
+            </div>
+            <div class="confname" >
+                <h1><?php echo htmlspecialchars($name); ?></h1>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
